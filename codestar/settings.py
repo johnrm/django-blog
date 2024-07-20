b6731f135416ade474bb08a6b2bbd6127b862748
John Mangan <notjohnrm@gmail.com>
2024-07-20 16:02:40 +0000

    Add summernote

--- codestar/settings.py
+++ codestar/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     '8000-johnrm-djangoblog-xuim72f207q.ws.codeinstitute-ide.net',
